74bb8c22cc4792ee75241cb478affd74727f5e89
M00rphy <agpkkck@gmail.com>
2023-06-25 22:47:02 -0600

    fsd

--- admin/mng-admin/js/login.js
+++ admin/mng-admin/js/login.js
@@ -1,38 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("loaded")
-    document.getElementById('signin').addEventListener('submit', (e) => {
-        e.preventDefault()
-        console.log("submitted")
-        $.ajax({
-            url: "php/login.php",
-            type: "post",
-            data: {email: document.getElementById('email').value, password: document.getElementById('password').value},
-            cache: false,
-            processData: false,
-            contentType: false,
-            beforeSend: function () {
-                // DescButtonsV(formulario);
-            },
-            complete: function () {
-                // ActButtonsV(formulario);
-                // editorck = false;
-            },
-            error: function () {
-                alerta(
-                    "Error",
-                    "Ocurrió un error al conectarse con el servidor, inténtelo más tarde por favor",
-                    "error",
-                    "Aceptar"
-                );
-            },
-            success: function (response) {
-                console.log(response);
-                // alerta(response.title, response.msg, response.class, "Aceptar");
-                // if (response.success) formulario.trigger("reset");
-                // setTimeout(function () {
-                //     eval(response.final);
-                // }, 2000);
-            }
-        });
-    })
+
+})
+
+document.getElementById('signin').addEventListener('submit', (e) => {
+    // e.preventDefault()
+    console.log("clicked")
+    debugger
+    $.ajax({
+        url: "php/login.php",
+        type: "post",
+        data: {email: document.getElementById('email').value, password: document.getElementById('password').value},
+        cache: false,
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+            // DescButtonsV(formulario);
+        },
+        complete: function () {
+            // ActButtonsV(formulario);
+            // editorck = false;
+        },
+        error: function () {
+            alerta(
+                "Error",
+                "Ocurrió un error al conectarse con el servidor, inténtelo más tarde por favor",
+                "error",
+                "Aceptar"
+            );
+        },
+        success: function (response) {
+            console.log(response);
+            // alerta(response.title, response.msg, response.class, "Aceptar");
+            // if (response.success) formulario.trigger("reset");
+            // setTimeout(function () {
+            //     eval(response.final);
+            // }, 2000);
+        }
+    });
+})
+
+
+document.getElementById('signin').addEventListener('type', (e) => {
+    // e.preventDefault()
+    console.log("clicked")
+    debugger
+    $.ajax({
+        url: "php/login.php",
+        type: "post",
+        data: {email: document.getElementById('email').value, password: document.getElementById('password').value},
+        cache: false,
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+            // DescButtonsV(formulario);
+        },
+        complete: function () {
+            // ActButtonsV(formulario);
+            // editorck = false;
+        },
+        error: function () {
+            alerta(
+                "Error",
+                "Ocurrió un error al conectarse con el servidor, inténtelo más tarde por favor",
+                "error",
+                "Aceptar"
+            );
+        },
+        success: function (response) {
+            console.log(response);
+            // alerta(response.title, response.msg, response.class, "Aceptar");
+            // if (response.success) formulario.trigger("reset");
+            // setTimeout(function () {
+            //     eval(response.final);
+            // }, 2000);
+        }
+    });
 })
--- admin/php/conection.php
+++ admin/php/conection.php
@@ -8,7 +8,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'lx_cv');
+define('DB_NAME', 'lx');
 # conectare la base de datos
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
